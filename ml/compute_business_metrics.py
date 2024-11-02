d4425e87b1564e54e32ab84c5c2a530f29e93900
marulyanova <devchatay@gmail.com>
2024-11-02 23:04:03 +0300

    [ml]: business metrics ready

--- ml/compute_business_metrics.py
+++ ml/compute_business_metrics.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
+import os
 from typing import List, Tuple
 
-code_column = "Код стр."
+code_column = "Код"
+
 
 """
 ликвидность done
@@ -22,14 +24,14 @@ ROE done
 
 def read_excel(path_to_excel: str) -> Tuple[List[pd.DataFrame], int]:
 
-    sheets = pd.ExcelFile(path_to_excel).sheet_names
+    sheets = pd.ExcelFile(path_to_excel).sheet_names[:2]
     xls = []
     n_start_row = pd.read_excel(path_to_excel)
     year = int(n_start_row["Unnamed: 3"][4])
     n_start_row = n_start_row[
         (n_start_row["Unnamed: 0"] == "АКТИВ")
         | (n_start_row["Unnamed: 0"] == "Наименование показателя")
-    ].index[0]
+    ].index.min()
 
     for sheet in sheets:
         df = pd.read_excel(
@@ -39,6 +41,7 @@ def read_excel(path_to_excel: str) -> Tuple[List[pd.DataFrame], int]:
         # Переименование колонок
         new_column_names = df.columns.tolist()
 
+        new_column_names[1] = "Код"
         if len(new_column_names) > 2:
             new_column_names[2] = "1"
         if len(new_column_names) > 3:
@@ -48,14 +51,16 @@ def read_excel(path_to_excel: str) -> Tuple[List[pd.DataFrame], int]:
 
         df.columns = new_column_names
 
-        df.replace("-", np.nan, inplace=True)
+        df.iloc[:, 1:] = df.iloc[:, 1:].applymap(
+            lambda x: np.nan if isinstance(x, str) and not x[0].isdigit() else x
+        )
         df.iloc[:, 1:] = df.iloc[:, 1:].astype(float)
         xls += [df]
     return xls, year
 
 
 # Ликвидность текущая, быстрая, абсолютная
-def get_liquidity(df: List[pd.DataFrame]) -> List[float]:
+def get_liquidity(df: List[pd.DataFrame], year: int) -> pd.DataFrame:
     """
     Текущая ликвидность = стр. 1200 / стр. 1500 где:
     Стр. 1200 — номер строки итога раздела II «Оборотные активы» бухгалтерского баланса;
@@ -68,66 +73,110 @@ def get_liquidity(df: List[pd.DataFrame]) -> List[float]:
 
     """
 
-    data = df[1]
-    return (
-        data[data[code_column] == 1200]["1"].values[0]
-        / data[data[code_column] == 1500]["1"].values[0],
-        (
-            data[data[code_column] == 1230]["1"].values[0]
-            + data[data[code_column] == 1240]["1"].values[0]
-            + data[data[code_column] == 1250]["1"].values[0]
-        )
-        / (
-            data[data[code_column] == 1510]["1"].values[0]
-            + data[data[code_column] == 1520]["1"].values[0]
-            + data[data[code_column] == 1550]["1"].values[0]
-        ),
-        (
-            data[data[code_column] == 1250]["1"].values[0]
-            + data[data[code_column] == 1240]["1"].values[0]
-        )
-        / (
-            data[data[code_column] == 1510]["1"].values[0]
-            + data[data[code_column] == 1520]["1"].values[0]
-            + data[data[code_column] == 1550]["1"].values[0]
-        ),
+    data = df[0]
+
+    def compute(year: str) -> List[float]:
+        return [
+            data[data[code_column] == 1200][year].values[0]
+            / data[data[code_column] == 1500][year].values[0],
+            (
+                data[data[code_column] == 1230][year].values[0]
+                + data[data[code_column] == 1240][year].values[0]
+                + data[data[code_column] == 1250][year].values[0]
+            )
+            / (
+                data[data[code_column] == 1510][year].values[0]
+                + data[data[code_column] == 1520][year].values[0]
+                + data[data[code_column] == 1550][year].values[0]
+            ),
+            (
+                data[data[code_column] == 1250][year].values[0]
+                + data[data[code_column] == 1240][year].values[0]
+            )
+            / (
+                data[data[code_column] == 1510][year].values[0]
+                + data[data[code_column] == 1520][year].values[0]
+                + data[data[code_column] == 1550][year].values[0]
+            ),
+        ]
+
+    chart_df = pd.DataFrame(
+        {
+            "metric_name": [
+                "текущая ликвидность",
+                "быстрая ликвидность",
+                "абсолютная ликвидность",
+            ]
+            * 3,
+            "matric_value": compute("1") + compute("2") + compute("3"),
+            "year": [year] * 3 + [year - 1] * 3 + [year - 2] * 3,
+        }
     )
 
+    return chart_df
 
-# Рентабельность продаж
-def profitability_of_sales(df: List[pd.DataFrame]) -> List[float]:
+
+# Рентабельность продаж, Рентабельность собственного капитала
+def profitability_of_sales(df: List[pd.DataFrame], year: int) -> pd.DataFrame:
     """
     Рентабельность продаж по валовой прибыли = строка 2100 / строка 2110 × 100
     Рентабельность продаж по операционной прибыли = (строка 2300 строка 2330) / строка 2110 × 100
     Рентабельность продаж по чистой прибыли = строка 2400 / строка 2110 × 100
+
+    Рентабельность собственного капитала = стр. 2400/ стр. 1300 × 100. где:
+    Стр. 2400 -строка отчета о финансовых результатах (чистая прибыль компании);
+    Стр. 1300 — строка бухгалтерского баланса (итоговая строка раздела III «Капитал и резервы»).
+
     """
 
-    data = df[1]
-    return [
-        data[data[code_column] == 2100]["1"].values[0]
-        / data[data[code_column] == 2110]["1"].values[0]
-        * 100,
-        (
-            data[data[code_column] == 2300]["1"].values[0]
-            + data[data[code_column] == 2330]["1"].values[0]
-        )
-        / data[data[code_column] == 2110]["1"].values[0]
-        * 100,
-        data[data[code_column] == 2400]["1"].values[0]
-        / data[data[code_column] == 2110]["1"].values[0]
-        * 100,
-    ]
+    data_1, data_2 = df[0], df[1]
+
+    def compute(year: str) -> List[float]:
+        return [
+            data_2[data_2[code_column] == 2100][year].values[0]
+            / data_2[data_2[code_column] == 2110][year].values[0]
+            * 100,
+            (
+                data_2[data_2[code_column] == 2300][year].values[0]
+                + data_2[data_2[code_column] == 2330][year].values[0]
+            )
+            / data_2[data_2[code_column] == 2110][year].values[0]
+            * 100,
+            data_2[data_2[code_column] == 2400][year].values[0]
+            / data_2[data_2[code_column] == 2110][year].values[0]
+            * 100,
+            data_2[data_2[code_column] == 2400][year].values[0]
+            / data_1[data_1[code_column] == 1300][year].values[0]
+            * 100,
+        ]
+
+    chart_df = pd.DataFrame(
+        {
+            "metric_name": [
+                "рентабельность продаж по валовой прибыли",
+                "рентабельность продаж по операционной прибыли",
+                "рентабельность продаж по чистой прибыли",
+                "рентабельность собственного капитала",
+            ]
+            * 2,
+            "matric_value": compute("1") + compute("2"),
+            "year": [year] * 4 + [year - 1] * 4,
+        }
+    )
+
+    return chart_df
 
 
-# ROA, Рентабельность оборотных активов, Рентабельность внеоборотных активов, Рентабельность собственного капитала
-def profitability_of_assets(df: List[pd.DataFrame], profit_type="sales") -> List[float]:
+# ROE (Рентабельность активов), Рентабельность оборотных активов, Рентабельность внеоборотных активов, Рентабельность собственного капитала
+def profitability_of_assets(
+    df: List[pd.DataFrame], year: int, profit_type="sales"
+) -> List[float]:
     """
-    Рентабельности активов = прибыль за период / средняя величина активов за период х 100%
+    Рентабельности активов (ROE) = прибыль за период / средняя величина активов за период х 100%
     Показатели прибыли для числителя формулы рентабельности активов нужно взять из отчета о финансовых результатах:
     прибыль от продаж — из строки 2200;
     чистую прибыль — из строки 2400.
-
-    Рентабельность собственного капитала = стр. 2400/ стр. 1300 × 100.
+    Средняя величина активов = (Сальдо баланса на начало периода + Сальдо баланса на конец периода) / 2, где сальдо баланса берется из строки 1600.
 
     Рентабельность оборотных активов = (2400 или 2200) / средняя величина оборотных активов * 100%
     Средняя величина оборотных активов = (Итог раздела II актива баланса на начало периода + Итог раздела II актива баланса на конец периода) / 2, берется из строки 1200.
@@ -147,43 +196,69 @@ def profitability_of_assets(df: List[pd.DataFrame], profit_type="sales") -> List
         raise ValueError("incorrect type of profit! Choose sales or net_profit")
 
     data_1, data_2 = df[0], df[1]
-    return [
-        data_2[data_2[code_column] == profit_code]["1"].values[0]
-        / (
-            (
-                data_1[data_1[code_column] == 1600]["1"].values[0]
-                + data_1[data_1[code_column] == 1600]["2"].values[0]
+
+    def compute(year: str) -> List[float]:
+
+        year_prev = str(int(year) + 1)
+
+        return [
+            data_2[data_2[code_column] == profit_code]["1"].values[0]
+            / (
+                (
+                    data_1[data_1[code_column] == 1600][year].values[0]
+                    + data_1[data_1[code_column] == 1600][year_prev].values[0]
+                )
+                / 2
             )
-            / 2
-        )
-        * 100,
-        data_2[data_2[code_column] == profit_code]["1"].values[0]
-        / (
-            (
-                data_1[data_1[code_column] == 1200]["1"].values[0]
-                + data_1[data_1[code_column] == 1200]["2"].values[0]
+            * 100,
+            data_2[data_2[code_column] == profit_code][year].values[0]
+            / (
+                (
+                    data_1[data_1[code_column] == 1200][year].values[0]
+                    + data_1[data_1[code_column] == 1200][year_prev].values[0]
+                )
+                / 2
             )
-            / 2
-        )
-        * 100,
-        data_2[data_2[code_column] == profit_code]["1"].values[0]
-        / (
-            (
-                data_1[data_1[code_column] == 1100]["1"].values[0]
-                + data_1[data_1[code_column] == 1100]["2"].values[0]
+            * 100,
+            data_2[data_2[code_column] == profit_code][year].values[0]
+            / (
+                (
+                    data_1[data_1[code_column] == 1100][year].values[0]
+                    + data_1[data_1[code_column] == 1100][year_prev].values[0]
+                )
+                / 2
             )
-            / 2
-        )
-        * 100,
-    ]
+            * 100,
+        ]
+
+    chart_df = pd.DataFrame(
+        {
+            "metric_name": [
+                "рентабельность всех активов",
+                "рентабельность оборотных активов",
+                "рентабельность внеоборотных активов",
+            ]
+            * 2,
+            "matric_value": compute("1") + compute("2"),
+            "year": [year] * 3 + [year - 1] * 3,
+        }
+    )
+
+    return chart_df
 
 
 def main():
-    df, year = read_excel("excel_data/МТС_2011.xlsx")
-    # res1 = profitability_of_sales(df)
-    # res2 = profitability_of_assets(df)
-    # print(res1)
-    # print(res2)
+    for file in os.listdir("excel_data"):
+        df, year = read_excel("excel_data/" + file)
+        metrics_for_chart = pd.concat(
+            [
+                get_liquidity(df, year),
+                profitability_of_sales(df, year),
+                profitability_of_assets(df, year),
+            ]
+        )
+
+        print(metrics_for_chart)
 
 
 if __name__ == "__main__":
