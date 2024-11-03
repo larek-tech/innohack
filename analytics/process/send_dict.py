f4dcab65e6a915fa9bb49fa36a66e8394a78a390
anyhhope <hopeanysimova@yandex.ru>
2024-11-04 01:56:02 +0300

    some changes

---
+++ analytics/process/send_dict.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+import json
+
+mongo = MongoClient("mongodb://46.138.243.191:27017/data", timeoutMS=30000**2)
+
+summary_col = mongo.get_database("data").get_collection("report_summary")
+
+def send_dict(summary_dict: dict):
+    has_summary_dict = len([s for s in summary_col.find({})]) > 0
+    if not has_summary_dict:
+        summary_col.insert_one(summary_dict)
+        print("Sent")
+
+def main():
+    path = "/summary_json.json"
+    f = open(path)
+    data = json.load(f)
+
+    send_dict(data)
+
+main()
